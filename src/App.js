e3937b4a7eced254c0511e780dce0d491111655d
Dmytro Kyrychyk <maestrokd.reg@gmail.com>
2025-05-24 11:18:45 -0700

    feat: GH-53 - BE connection test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { fetchDataFromAPI } from './services/apiService';
+import {fetchDataFromAPI, fetchDataFromAPI2, fetchDataFromAPI3, fetchDataFromAPI4, fetchBeStatus, fetchMessage} from './services/apiService';
 
 function App() {
   const [userId, setUserId] = useState(''); // State to hold the user ID
+  const [host, setHost] = useState(''); // State to hold the user ID
+  const [message, setMessage] = useState(''); // State to hold the user ID
   const [data, setData] = useState({ categories: [], subcategories: {} }); // State to hold the API data
   const [loading, setLoading] = useState(false); // State to manage loading state
   const [error, setError] = useState(null); // State to manage error state
+  // const [logs, setLogs] = useState([]); // State to hold log messages
+  const [beStatus, setBeStatus] = useState(null);
+
+  // Custom logging function
+  // const console.log = (message) => {
+  //   setLogs(prevLogs => [...prevLogs, `${new Date().toISOString()}: ${message}`]);
+  //   // Also log to browser console
+  //   console.log(message);
+  // };
 
   const handleFetchData = () => {
+    setError(null);
+    setData({ categories: [], subcategories: {} });
+    console.log(`Fetching data for user: ${userId}`);
     setLoading(true);
-    fetchDataFromAPI(userId)
+    fetchDataFromAPI(userId, console.log)
         .then(responseData => {
+          console.log(`Data received successfully`);
           setData(responseData); // Set both categories and subcategories map
           setLoading(false);
         })
         .catch(error => {
+          console.log(`Error: ${error.message}`);
+          setError(error);
+          setLoading(false);
+        });
+
+    fetchDataFromAPI3(console.log)
+        .then(responseData => {
+          console.log(`3 Data received successfully`);
+        })
+        .catch(error => {
+          console.log(`3 Error: ${error.message}`);
+        });
+
+    fetchDataFromAPI4(console.log)
+        .then(responseData => {
+          console.log(`4 Data received successfully`);
+        })
+        .catch(error => {
+          console.log(`4 Error: ${error.message}`);
+        });
+  };
+
+  const handleFetchData2 = () => {
+    setError(null);
+    setData({ categories: [], subcategories: {} });
+    console.log(`Fetching data for user: ${userId}, host: ${host}`);
+    setLoading(true);
+    fetchDataFromAPI2(host, userId, console.log).then(userId, console.log)
+        .then(responseData => {
+          console.log(`Data received successfully`);
+          setData(responseData); // Set both categories and subcategories map
+          setLoading(false);
+        })
+        .catch(error => {
+          console.log(`Error: ${error.message}`);
           setError(error);
           setLoading(false);
         });
   };
+
+  const handleFetchBeStatus = () => {
+    setBeStatus(null);
+    setLoading(true);
+    fetchBeStatus(console.log)
+        .then(responseData => {
+          console.log(`BE status received successfully`);
+          setBeStatus(responseData);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.log(`BE status error: ${error.message}`);
+          setError(error);
+          setLoading(false);
+        });
+  };
+
+  const handleFetchMessage = () => {
+    console.log(`Fetching message: ${userId}`);
+    setLoading(true);
+    fetchMessage(userId, console.log)
+        .then(responseData => {
+          console.log(`Data received successfully ${responseData}`);
+            setMessage(responseData); // Set both categories and subcategories map
+          setLoading(false);
+        })
+        .catch(error => {
+          console.log(`Error: ${error.message}`);
+          setError(error);
+          setLoading(false);
+        });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -29,9 +112,15 @@ function App() {
         <h2>
           GH Pages Demo
         </h2>
-        <h2>
-          dev codebase
-        </h2>
+
+        <h3>BE status</h3>
+        <p>
+          <button onClick={handleFetchBeStatus}>Refresh</button>
+        </p>
+        <p>
+          {beStatus ? JSON.stringify(beStatus) : 'No BE status'}
+        </p>
+
 
         <p> 2 Test env var: {process.env.REACT_APP_TEST_VAR} for {process.env.NODE_ENV} env.</p>
         <p> 3 env var 2: {process.env.REACT_APP_BE_REST_HOST}</p>
@@ -51,16 +140,26 @@ function App() {
         <div>
           <input
               type="text"
+              value={host}
+              onChange={(e) => setHost(e.target.value)}
+              placeholder="Enter host"
+          />
+          <input
+              type="text"
               value={userId}
               onChange={(e) => setUserId(e.target.value)}
               placeholder="Enter user ID"
           />
-          <button onClick={handleFetchData}>Fetch Categories and Subcategories</button>
+          <button onClick={handleFetchData}>Fetch Cat</button>
+          <button onClick={handleFetchData2}>Fetch Cat 2</button>
+          <button onClick={handleFetchMessage}>Message</button>
         </div>
 
         {loading && <p>Loading data...</p>}
         {error && <p>Error fetching data: {error.message}</p>}
-        {data.categories.length > 0 && (
+        {message && <p>BE message: {message}</p>}
+
+        {data && data.categories && data.categories.length > 0 && (
             <div>
               <h3>Categories:</h3>
               <ul>
@@ -79,6 +178,25 @@ function App() {
               </ul>
             </div>
         )}
+
+        {/* Log display area */}
+        {/*<div className="console-output">
+          <h3>Console Output:</h3>
+          <div style={{
+            backgroundColor: '#f0f0f0',
+            padding: '10px',
+            borderRadius: '5px',
+            fontFamily: 'monospace',
+            maxHeight: '200px',
+            overflowY: 'auto',
+            textAlign: 'left',
+            color: '#333'
+          }}>
+            {logs.map((log, index) => (
+                <div key={index}>{log}</div>
+            ))}
+          </div>
+        </div>*/}
       </header>
     </div>
   );
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,19 +1,56 @@
-export const fetchDataFromAPI = async (userId) => {
+export const createApiUrlBase = (path, params = {}) => {
+    const host = process.env.REACT_APP_BE_REST_HOST;
+    return createApiUrl(host, path, params);
+};
+
+export const createApiUrl = (host, path, params = {}) => {
+    const url = new URL(path, host);
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+    return url.toString();
+};
+
+export const fetchApi = async (url, options = {}) => {
     try {
-        //const url = `http://localhost:8080/budget/categories?userId=${userId}`;
-        //const url = `http://194.26.232.35:8083/budget/categories?userId=158602720`;
-        const host = process.env.REACT_APP_BE_REST_HOST;
-        console.log("host: " + host);
-        // const url = `http://194.26.232.35:8083/budget/categories?userId=${userId}`;
-        const url = `https://` + host + `/budget/categories?userId=${userId}`;
-        console.log("URL: " + url);
-        const response = await fetch(url);
+        const response = await fetch(url, options);
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-        const data = await response.json();
-        return data; // Return the entire response containing categories and subcategories map
+        const data = await response;
+        console.log("Received Data: " + data);
+        return data;
     } catch (error) {
+        console.log("Error in API call. message: " + error.message);
+        console.log("Error in API call. error: " + error.stack);
         throw error;
     }
+};
+
+export const fetchDataFromAPI = async (userId) => {
+    const url = createApiUrlBase("/budget/categories", { userId });
+    return fetchApi(url).then(response => response.json());
+};
+
+export const fetchDataFromAPI2 = async (host, userId) => {
+    const url = createApiUrl(host, "/budget/categories", { userId: userId });
+    return fetchApi(url).then(response => response.json());
+};
+
+export const fetchDataFromAPI3 = async () => {
+    const url = "https://api.restful-api.dev/objects/7";
+    return fetchApi(url).then(response => response.json());
+};
+
+export const fetchDataFromAPI4 = async () => {
+    const url = "https://194.26.232.35/budget/categories2";
+    return fetchApi(url).then(response => response.json());
+};
+
+export const fetchBeStatus = async () => {
+    const url = "https://prdev.be.familybudgetbot.xyz/health";
+    return fetchApi(url).then(response => response.json());
+};
+
+export const fetchMessage = async (message="") => {
+    const url = createApiUrlBase("/health/message", { message: message });
+    return fetchApi(url).then(response => response.text());
 };
